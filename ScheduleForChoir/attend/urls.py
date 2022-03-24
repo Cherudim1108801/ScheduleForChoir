561138f8a533389c70772fcafcf0ed5ff649e612
KOHEI-ThinkPadX240 <tomikou11088011@gmail.com>
2022-03-24 21:41:44 +0900

    アプリにした

--- ScheduleForChoir/attend/urls.py
+++ ScheduleForChoir/attend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.topmenu, name="topmenu"),
+    path('', views.attend, name='attend')
 ]
--- ScheduleForChoir/topmenu/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def topmenu(request):
-    return HttpResponse("Hello,World. You're at the top menu.")
