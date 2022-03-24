12e30c32d914125a910681c179d8787399343e1b
KOHEI-ThinkPadX240 <tomikou11088011@gmail.com>
2022-03-24 15:41:17 +0900

    バグFIX

--- ScheduleForChoir/ScheduleForChoir/urls.py
+++ ScheduleForChoir/ScheduleForChoir/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('topmanu', include('topmenu.urls')),
+    path('topmanu/', include('topmenu.urls')),
     path('admin/', admin.site.urls),
 ]
